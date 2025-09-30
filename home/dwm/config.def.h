b663854123f728a399c664c2d05229f46d44c981
Ezequiel <ezequiel920@proton.me>
2025-09-30 17:48:32 +0000

    Add files via upload

---
+++ home/dwm/config.def.h
@@ -0,0 +1,175 @@
+/* See LICENSE file for copyright and license details. */
+
+/* Personal */
+#define TERMINAL "st"
+#define TERMCLASS "St"
+void view_adjacent(const Arg *arg);
+
+/* alt-tab configuration */
+static const unsigned int tabModKey 		= 0x40;	/* if this key is hold the alt-tab functionality stays acitve. This key must be the same as key that is used to active functin altTabStart `*/
+static const unsigned int tabCycleKey 		= 0x17;	/* if this key is hit the alt-tab program moves one position forward in clients stack. This key must be the same as key that is used to active functin altTabStart */
+static const unsigned int tabPosY 			= 1;	/* tab position on Y axis, 0 = bottom, 1 = center, 2 = top */
+static const unsigned int tabPosX 			= 1;	/* tab position on X axis, 0 = left, 1 = center, 2 = right */
+static const unsigned int maxWTab 			= 600;	/* tab menu width */
+static const unsigned int maxHTab 			= 200;	/* tab menu height */
+
+/* appearance */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "JetBrainsMono-Regular:size=14", "fontawesome:size=40" };
+static const char dmenufont[]       = "JetBrainsMono-Regular:size=12";
+static const char col_bg[]     = "#282828";  // Gruvbox dark background
+static const char col_fg[]     = "#ebdbb2";  // Gruvbox light text
+static const char col_border[] = "#1d2021";  // Darker gray
+static const char col_accent[] = "#d3869b";  // gruvbox-style pink
+
+static const char *colors[][3] = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_fg,    col_bg,    col_border },
+	[SchemeSel]  = { col_bg,    col_accent, col_accent },
+};
+
+/* tagging */
+static const char *tags[] = { "", "", "", "", "", "", "", "" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "discord",                 NULL,       NULL,       1 << 3,       0,            1 },
+	{ "librewolf",               NULL,       NULL,       1 << 2,       0,            1 },
+	{ "Spotify",                 NULL,       NULL,       1 << 4,       0,            1 },
+	{ "steam",    	             NULL,       NULL,       1 << 5,       0,            1 },
+	{ "Minecraft* 1.21.4",       NULL,       NULL,       1 << 5,       0,            1 },
+	{ "PrismLauncher",           NULL,       NULL,       1 << 5,       0,            1 },
+
+
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "[@]",      spiral },
+	{ NULL,       NULL },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = {
+    "dmenu_run", "-hp", "thorium-browser", "-m", dmenumon, "-fn", dmenufont,
+    "-nb", col_bg, "-nf", col_fg, "-sb", col_accent, "-sf", col_bg, NULL
+};
+static const char *termcmd[]  = { "st", NULL };
+static const char *webbr[]  = { "librewolf", NULL };
+static const char *fm[]  = { "pcmanfm", NULL };
+static const char *screencap[] = { "/home/nodis/.scripts/screencap.sh", NULL };
+static const char *screencap2[] = { "/home/nodis/.scripts/screencap2.sh", NULL };
+static const char *powermenu[] = { "/home/nodis/.scripts/powermenu.sh", NULL };
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_q,      spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = webbr } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = fm } },
+	{ MODKEY,						XK_o,      spawn,          {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
+	{ 0,							XK_Print,  spawn,          {.v = screencap } },
+	{ MODKEY, 						XK_Print,  spawn,  		   {.v = screencap2 } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = (const char*[]){ "/home/nodis/.scripts/volumectl", "up",   NULL } } },
+	{ MODKEY,                       XK_F10,    spawn,          {.v = (const char*[]){ "/home/nodis/.scripts/volumectl", "down", NULL } } },
+	{ MODKEY|ShiftMask, 			XK_p,      spawn,          {.v = powermenu } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,						XK_Right, view_adjacent, {.i = +1 } }, // Move to the next tag
+	{ MODKEY, 						XK_Left,  view_adjacent, {.i = -1 } }, // Move to the previous tags
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,         			    XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
+	{ MODKEY|Mod1Mask,              XK_space,  setlayout, {0} },
+	{ MODKEY,			            XK_f,      togglefullscr,  {0} },
+	{ Mod1Mask,             		XK_Tab,    altTabStart,	   {0} },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
